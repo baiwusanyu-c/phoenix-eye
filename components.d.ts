fed4dd7c8f8c72a82e689a8a496806d1aa454c6e
czh740132583 <740132583@qq.com>
2022-03-28 18:42:52 +0800

    chore: 临时提交

--- components.d.ts
+++ components.d.ts
@@ -4,24 +4,7 @@
 
 declare module 'vue' {
   export interface GlobalComponents {
-    ElButton: typeof import('element-plus/es')['ElButton']
-    ElDialog: typeof import('element-plus/es')['ElDialog']
-    ElForm: typeof import('element-plus/es')['ElForm']
-    ElFormItem: typeof import('element-plus/es')['ElFormItem']
-    ElInput: typeof import('element-plus/es')['ElInput']
-    EllipsisCopy: typeof import('./src/components/common-components/ellipsis-copy/ellipsis-copy.vue')['default']
-    ElMenu: typeof import('element-plus/es')['ElMenu']
-    ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
-    ElOption: typeof import('element-plus/es')['ElOption']
-    ElSelect: typeof import('element-plus/es')['ElSelect']
-    ElTable: typeof import('element-plus/es')['ElTable']
-    ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
-    ElTooltip: typeof import('element-plus/es')['ElTooltip']
     FeedBack: typeof import('./src/components/feed-back.vue')['default']
-    Loading: typeof import('element-plus/es')['ElLoadingDirective']
-    MsgDialog: typeof import('./src/components/common-components/msg-dialog/msg-dialog.vue')['default']
-    TsgzFooter: typeof import('./src/components/tsgz-footer.vue')['default']
-    TsgzNavMenu: typeof import('./src/components/tsgz-nav-menu.vue')['default']
   }
 }
 
