15f7922de82836a3369856c4563c53871b6b4bf9
czh740132583 <740132583@qq.com>
2022-03-30 19:07:01 +0800

    Merge remote-tracking branch 'origin/develop' into dev-czh
    # Conflicts:
    #	public/be-ui/be-ui.es.js
    #	public/be-ui/style.css
    #	src/utils/mixin/common-func.tsx

--- components.d.ts
+++ components.d.ts
@@ -5,7 +5,6 @@
 declare module 'vue' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
-    ElDialog: typeof import('element-plus/es')['ElDialog']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElInput: typeof import('element-plus/es')['ElInput']
@@ -13,7 +12,6 @@ declare module 'vue' {
     ElMenu: typeof import('element-plus/es')['ElMenu']
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
     ElOption: typeof import('element-plus/es')['ElOption']
-    ElPagination: typeof import('element-plus/es')['ElPagination']
     ElSelect: typeof import('element-plus/es')['ElSelect']
     ElTable: typeof import('element-plus/es')['ElTable']
     ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
@@ -25,4 +23,4 @@ declare module 'vue' {
   }
 }
 
-export {}
+export { }
