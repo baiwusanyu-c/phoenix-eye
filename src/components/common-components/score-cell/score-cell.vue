9846182368e3f0c4bd18aadbe5526eb15f880ff3
czh740132583 <740132583@qq.com>
2022-05-18 11:00:32 +0800

    feat: 新增scoreCell组件封装

---
+++ src/components/common-components/score-cell/score-cell.vue
@@ -0,0 +1,79 @@
+/* * @score-cell.vue * @deprecated * @author czh * @update (czh 2022/5/18) */
+<template>
+  <div class="score-cell">
+    <be-progress
+      type="circle"
+      :percent="score"
+      :color="color"
+      status="normal"
+      trail-color="#E0E0E0"
+      stroke-linecap="square"
+      stroke-width="20">
+      <template #center>
+        <span class="score">{{ score === 0 ? 'N/A' : score }}</span>
+      </template>
+    </be-progress>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent, ref } from 'vue'
+  // @ts-ignore
+  import { BeProgress } from '../../../../public/be-ui/be-ui.es'
+
+  export default defineComponent({
+    name: 'ScoreCell',
+    components: {
+      BeProgress,
+    },
+    props: {
+      score: {
+        type: Number,
+        default: 0,
+      },
+      level: {
+        type: String,
+        default: '3',
+      },
+    },
+    setup(props) {
+      const color = ref<string>('')
+      if (props.level === '1') {
+        color.value = '#0ED9AC'
+      }
+      if (props.level === '2') {
+        color.value = '#F4CC29'
+      }
+      if (props.level === '3') {
+        color.value = '#F32F2F'
+      }
+      const hiddenPath = ref<string>('')
+      if (props.score === 0) {
+        hiddenPath.value = 'none'
+      }
+      return {
+        color,
+        hiddenPath,
+      }
+    },
+  })
+</script>
+
+<style lang="scss">
+  .score-cell {
+    .be-progress-body {
+      width: 40px !important;
+      height: 40px !important;
+    }
+    .be-progress-circle--text {
+      width: 100%;
+    }
+    .be-progress-circle__path {
+      display: v-bind(hiddenPath);
+    }
+    .score {
+      font-weight: bold;
+      font-size: 12px;
+    }
+  }
+</style>
