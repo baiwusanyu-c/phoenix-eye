7e2ab0c1ff33140e823c3e0e833560cad74d9df9
czh740132583 <740132583@qq.com>
2022-05-26 10:45:41 +0800

    feat: 项目浏览器risk-line.vue

---
+++ src/components/common-components/area-line-cell/area-line-cell.vue
@@ -0,0 +1,206 @@
+/* * @area-line-cell.vue * @deprecated * @author czh * @update (czh 2022/5/25) */
+<template>
+  <div :id="domId"></div>
+</template>
+
+<script lang="ts">
+  import { defineComponent, nextTick, onMounted, ref, watch } from 'vue'
+  import { Chart } from '@antv/g2'
+  import { formatMoney } from '../../../utils/common'
+  import type { PropType } from 'vue'
+  import type { IStatisticsLine } from '../../../utils/types'
+  export default defineComponent({
+    name: 'AreaLineCell',
+    props: {
+      domId: {
+        type: String,
+        default: 'line-cell_domId',
+      },
+      height: {
+        type: Number,
+        default: 300,
+      },
+      lineData: {
+        type: Array as PropType<Array<IStatisticsLine>>,
+      },
+      smooth: {
+        type: Boolean,
+        default: true,
+      },
+      x: {
+        type: String,
+        default: 'token_price',
+      },
+      y: {
+        type: String,
+        default: 'create_time',
+      },
+    },
+    setup(props) {
+      onMounted(() => {
+        nextTick(() => {
+          renderChart()
+        })
+      })
+      // 雷达图实例对象
+      const chart = ref<any>()
+      watch(
+        () => props.lineData,
+        () => {
+          renderChart(true)
+        }
+      )
+      const renderChart = (isUpdate?: boolean) => {
+        const data = [
+          { create_time: '1991', token_price: 3 },
+          { create_time: '1992', token_price: 4 },
+          { create_time: '1993', token_price: 3.5 },
+          { create_time: '1994', token_price: 5 },
+          { create_time: '1995', token_price: 4.9 },
+          { create_time: '1996', token_price: 2 },
+          { create_time: '1997', token_price: 7 },
+          { create_time: '1998', token_price: 3 },
+          { create_time: '1999', token_price: 4 },
+          { create_time: '2000', token_price: 3.5 },
+          { create_time: '2001', token_price: 5 },
+          { create_time: '2002', token_price: 4.9 },
+          { create_time: '2003', token_price: 2 },
+          { create_time: '2004', token_price: 7 },
+          { create_time: '2005', token_price: 3 },
+          { create_time: '2006', token_price: 4 },
+          { create_time: '2007', token_price: 3.5 },
+          { create_time: '2008', token_price: 5 },
+          { create_time: '2009', token_price: 4.9 },
+          { create_time: '2010', token_price: 2 },
+          { create_time: '2011', token_price: 7 },
+          { create_time: '2012', token_price: 7 },
+          { create_time: '2013', token_price: 7 },
+          { create_time: '2014', token_price: 7 },
+          { create_time: '2015', token_price: 7 },
+          { create_time: '2016', token_price: 7 },
+          { create_time: '2017', token_price: 7 },
+          { create_time: '2018', token_price: 7 },
+          { create_time: '2019', token_price: 7 },
+          { create_time: '2020', token_price: 7000 },
+        ]
+        // 更新
+        if (isUpdate) {
+          chart.value.data(props.lineData)
+          chart.value.render(isUpdate)
+          return
+        }
+        chart.value = new Chart({
+          container: props.domId,
+          autoFit: true,
+          height: 360,
+        })
+
+        chart.value.data(data)
+        chart.value.scale({
+          token_price: {
+            min: 10000,
+            nice: true,
+          },
+          create_time: {
+            range: [0, 1],
+          },
+        })
+        chart.value.tooltip({
+          showCrosshairs: true,
+          showTitle: false,
+          showContent: true,
+          marker: {
+            fill: '#1A589B',
+          },
+          crosshairs: {
+            type: 'x',
+            line: {
+              style: {
+                stroke: 'l(90) 0:rgba(39, 114, 240, 0) 1:#1A589B32 ',
+                lineWidth: 46,
+              },
+            },
+          },
+          customContent: (name: any, items: Array<{ title: string; value: number }>) => {
+            const container = document.createElement('div')
+            container.className = 'g2-tooltip'
+            let listItem = ''
+            items.forEach((item: { title: string; value: number }) => {
+              listItem += `
+              <li class="g2-tooltip-list-item" style="margin-bottom:4px;">
+                  <p class="g2-tooltip-list-token">$ ${formatMoney(item.value)}</p>
+                   <p>${item.title}</p>
+              </li>`
+            })
+            container.innerHTML = listItem
+            return container
+          },
+        })
+        chart.value.axis(props.y, {
+          label: {
+            style: {
+              fill: '#8A96A3',
+            },
+          },
+          line: null,
+          tickLine: null,
+        })
+        chart.value.axis(props.x, {
+          label: {
+            style: {
+              fill: '#8A96A3',
+            },
+          },
+          grid: {
+            line: {
+              type: 'line',
+              style: {
+                lineDash: [4],
+              },
+            },
+          },
+        })
+
+        chart.value
+          .line()
+          .position(`${props.y}*${props.x}`)
+          .shape(props.smooth ? 'smooth' : '')
+          .style({ stroke: '#1A589B' })
+        chart.value
+          .area()
+          .position(`${props.y}*${props.x}`)
+          .style({ fill: 'l(270) 0:rgba(39, 114, 240, 0) 1:#1A589B' })
+          .shape(props.smooth ? 'smooth' : '')
+        chart.value.render()
+      }
+    },
+  })
+</script>
+
+<style lang="scss">
+  .g2-tooltip {
+    background-color: $textColor3 !important;
+    border-radius: 8px !important;
+    min-width: 110px !important;
+    .g2-tooltip-marker {
+      display: none !important;
+    }
+    .g2-tooltip-list-item {
+      p {
+        text-align: left !important;
+        font-family: BarlowSemi-R, sans-serif;
+        font-size: 12px;
+        font-weight: 400;
+        color: $textColor7 !important;
+        line-height: 16px;
+      }
+      .g2-tooltip-list-token {
+        font-size: 14px;
+        font-family: BarlowSemi-B, sans-serif;
+        font-weight: bold;
+        line-height: 20px;
+        color: $mainColor7 !important;
+      }
+    }
+  }
+</style>
---
+++ src/views/pc/project-search/components/risk-list.vue
@@ -0,0 +1,195 @@
+/* * @risk-list.vue * @deprecated * @author czh * @update (czh 2022/5/26) */
+<template>
+  <div class="risk-list">
+    <el-table ref="riskTrxList" v-loading="loading" tooltip-effect="light" :data="tableData">
+      <template #empty>
+        <empty-data content="lang.noRisk"></empty-data>
+      </template>
+      <el-table-column prop="tx_hash" align="center">
+        <template #header>
+          <span class="table-head">{{ $t('lang.riskConfig.tableHeader.txHash') }}</span>
+        </template>
+        <template #default="scope">
+          <ellipsis-copy
+            :target-str="scope.row.tx_hash"
+            :is-show-copy-btn="false"
+            styles="font-weight:bold;font-family:BarlowSemi-B, sans-serif;color:#18304E"
+            font-length="7"
+            end-length="7">
+          </ellipsis-copy>
+        </template>
+      </el-table-column>
+      <el-table-column prop="alert_level" align="center" width="110">
+        <template #header>
+          <span class="table-head">{{ $t('lang.riskConfig.tableHeader.level') }}</span>
+        </template>
+        <template #default="scope">
+          <span v-if="scope.row.alert_level === 'LOW'" class="level-low">Low</span>
+          <span v-if="scope.row.alert_level === 'HIGH'" class="level-high">High</span>
+          <span v-if="scope.row.alert_level === 'MEDIUM'" class="level-medium">Medium</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="risk_features" align="left">
+        <template #header>
+          <span class="table-head">{{ $t('lang.riskConfig.tableHeader.warningType') }}</span>
+        </template>
+        <template #default="scope">{{ scope.row.tx_time }} </template>
+      </el-table-column>
+      <el-table-column prop="tx_time" align="right">
+        <template #header>
+          <span class="table-head">{{ $t('lang.riskConfig.tableHeader.txTime') }}</span>
+        </template>
+        <template #default="scope">
+          <date-cell :time="scope.row.tx_time" :is-break="false"></date-cell>
+        </template>
+      </el-table-column>
+    </el-table>
+    <div class="front-page">
+      <div class="front-page--body">
+        <span>{{ pageParams.currentPage }} / {{ pageNum }}</span>
+        <div class="page-btn-group">
+          <div class="page-btn" @click="prevPage">◀</div>
+          <div class="page-btn" @click="nextPage">▶</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent, onMounted, ref } from 'vue'
+  import compositionPage from '../../../../utils/mixin/page-param'
+
+  import composition from '../../../../utils/mixin/common-func'
+  import { getProjWarning } from '../../../../api/risk-trx'
+  import EllipsisCopy from '../../../../components/common-components/ellipsis-copy/ellipsis-copy.vue'
+  // @ts-ignore
+  import { BeTag } from '../../../../../public/be-ui/be-ui.es'
+
+  export default defineComponent({
+    name: 'RiskList',
+    components: {
+      EllipsisCopy,
+    },
+    setup() {
+      /* const filterTypeItem = ref<Array<IFilterItem>>([
+            { label: 'Large outflow', val: 'Large outflow', isActive: false },
+            { label: 'Flash loan', val: 'Flash loan', isActive: false },
+            { label: 'Privileged operation', val: 'Privileged operation', isActive: false },
+            { label: 'Slump', val: 'Slump', isActive: false },
+        ])*/
+      const { pageParams, resetPageParam } = compositionPage()
+      const pageNum = ref<number>(0)
+      const nextPage = (): void => {
+        pageParams.value.currentPage!++
+        if (pageParams.value.currentPage! > pageNum.value) {
+          pageParams.value.currentPage! = pageNum.value
+        }
+        getList()
+      }
+      const prevPage = (): void => {
+        pageParams.value.currentPage!--
+        if (pageParams.value.currentPage! < 1) {
+          pageParams.value.currentPage! = 1
+        }
+        getList()
+      }
+      const { message, isEmpty } = composition()
+      const tableData = ref<object>([])
+      const loading = ref<boolean>(false)
+
+      /**
+       * 获取表格数据
+       */
+      const getList = (type?: string) => {
+        // 使用loading变量防抖
+        if (loading.value) {
+          return
+        }
+        loading.value = true
+        if (type === 'reset') {
+          resetPageParam(5, pageParams)
+        }
+        const params = {
+          page_num: pageParams.value.currentPage,
+          page_size: pageParams.value.pageSize,
+        }
+        getProjWarning(params)
+          .then((res: any) => {
+            if (res && res.success) {
+              tableData.value = res.data.page_infos
+              pageParams.value.total = res.data.total
+              pageNum.value = Math.ceil(res.data.total / pageParams.value.pageSize!)
+            } else {
+              tableData.value = []
+              resetPageParam()
+              // message('error', 'system error')
+            }
+            loading.value = false
+          })
+          .catch(err => {
+            message('error', err.message || err)
+            loading.value = false
+            console.error(err)
+          })
+      }
+
+      onMounted(() => {
+        getList('reset')
+      })
+      return {
+        tableData,
+        loading,
+        pageNum,
+        prevPage,
+        nextPage,
+        pageParams,
+      }
+    },
+  })
+</script>
+
+<style lang="scss">
+  .risk-list {
+    .el-table__row {
+      height: 54px;
+    }
+    width: 50%;
+    height: 388px;
+    position: relative;
+    .front-page {
+      .front-page--body {
+        justify-content: flex-end;
+        align-items: center;
+      }
+      margin-top: 16px;
+    }
+    .level-low {
+      font-size: 14px;
+      font-family: BarlowSemi-B, sans-serif;
+      font-weight: bold;
+      color: $mainColor3;
+      background-color: $mainColor16-025;
+      padding: 4px 12px;
+      border-radius: 30px;
+    }
+    .level-high {
+      font-size: 14px;
+      font-family: BarlowSemi-B, sans-serif;
+      font-weight: bold;
+      color: $lessColor4;
+      padding: 4px 12px;
+      background-color: $lessColor4-025;
+      border-radius: 30px;
+    }
+    .level-medium {
+      font-size: 14px;
+      font-family: BarlowSemi-B, sans-serif;
+      font-weight: bold;
+      padding: 4px 12px;
+      color: $mainColor19;
+      background-color: $mainColor23;
+      border-radius: 30px;
+    }
+  }
+</style>
